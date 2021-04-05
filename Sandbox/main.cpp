4cafe60f4af698969f16e0740c1ef62be0a2ce20
dmowmyh <mar4omishev@gmail.com>
2021-04-05 18:10:39 +0300

    Updated README

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -11,44 +11,44 @@ using namespace std::chrono;
 
 int main()
 {
-    //srand (time(NULL));
+    srand (time(NULL));
 
-    //size_t input_size = 1000000;
-    //HeapMedian<int> median(input_size);
+    size_t input_size = 1000000;
+    HeapMedian<int> median(input_size);
 
-    //std::vector<int> temp;
-    //for (size_t i = 0; i < input_size; i++)
-    //{
-        //auto random_number = rand()%input_size;
-        //temp.emplace_back(random_number);
-    //}
+    std::vector<int> temp;
+    for (size_t i = 0; i < input_size; i++)
+    {
+        auto random_number = rand()%input_size;
+        temp.emplace_back(random_number);
+    }
 
-    //{
-        //auto start = high_resolution_clock::now();
-        //std::vector<int> v;
-        //for (auto val: temp)
-        //{
-            //v.push_back(val);
-        //}
-        //std::nth_element(v.begin(), v.begin() + v.size()/2, v.end());
-        //auto stop = high_resolution_clock::now();
-        //auto duration = duration_cast<milliseconds>(stop - start);
-        //std::cout << duration.count() << std::endl;
-    //}
+    {
+        auto start = high_resolution_clock::now();
+        std::vector<int> v;
+        for (auto val: temp)
+        {
+            v.push_back(val);
+        }
 
-    //{
-        //auto start = high_resolution_clock::now();
-        //for (auto val: temp)
-        //{
-            //median.Insert(std::forward<int>(val));
-        //}
-        //auto stop = high_resolution_clock::now();
-        //auto duration = duration_cast<milliseconds>(stop - start);
-        //std::cout << duration.count() << std::endl;
-    //}
+        //For some reason this algorithm doesn't return the write median when the input is even size
+        //But it's pretty fast have to look it up suffers from recalculation
+        //std::nth_element();
+        std::sort(v.begin(), v.end());
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        std::cout << "std::sort: "<< duration.count() << " ms" << std::endl;
+    }
 
-    //HeapMedian<float> medianFloats({1.1, 2.5, 3.5, 3.3, 10.1});
-    //std::cout << medianFloats.GetMedian() << std::endl;
-    //
+    {
+        auto start = high_resolution_clock::now();
+        for (auto val: temp)
+        {
+            median.Insert(std::forward<int>(val));
+        }
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        std::cout << "my implementation: " << duration.count() << " ms" << std::endl;
+    }
 }
 
